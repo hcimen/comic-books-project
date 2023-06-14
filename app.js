859eebb1596f89db2b755a9a1eb3afb1ca7b65bc
Harun Cimen <haruncimen@gmail.com>
2023-06-13 21:33:19 -0400

    Update app.js

--- app.js
+++ app.js
@@ -29,8 +29,3 @@ app.use(passport.session());
 const routes = require('./routes/index-routes');
 app.use('/', routes)
 
-const port = 5000; 
-app.listen(port, () => {
-  console.log(`Server listening at http://localhost:${port}`);
-});
-
